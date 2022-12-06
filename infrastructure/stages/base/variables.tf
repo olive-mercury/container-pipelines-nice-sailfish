7d0d0b02eaf92ee45951703576d730648c9f0492
Jim Counts <jamesrcounts@outlook.com>
2022-12-06 15:44:33 +0000

    Fixup Variables

--- infrastructure/stages/base/variables.tf
+++ infrastructure/stages/base/variables.tf
@@ -1,10 +1,10 @@
-variable "ops_instance_id" {
-  description = "(Required) The instance ID for this project's ops resources."
+variable "ops_resource_group_name" {
+  description = "The name of the resource group for this project's ops resources."
   type        = string
 }
 
 variable "ops_subscription_id" {
-  description = "(Required) The subscription ID containing this project's ops resources"
+  description = "The subscription ID containing this project's ops resources"
   type        = string
 }
 
@@ -14,6 +14,6 @@ variable "resource_group_name" {
 }
 
 variable "subscription_id" {
-  description = "(Required) The Azure subscription ID for the solution environment."
+  description = "The Azure subscription ID for the solution environment."
   type        = string
 }
